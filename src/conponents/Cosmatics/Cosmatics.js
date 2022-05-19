b0d7e9feebe0046a42c52708d8b07bbbe4427d8d
armancse-dev <armanhossaincse7@gmail.com>
2022-05-19 21:32:16 +0600

    import, export, default, relative path

---
+++ src/conponents/Cosmatics/Cosmatics.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { add, multiply } from '../../utilities/storage';
+
+const Cosmatics = () => {
+   const first = 55;
+   const second = 89;
+   const sum = add(first, second);
+   const multi = multiply (first, second);
+   
+   return (
+      <div>
+         
+      </div>
+   );
+};
+
+export default Cosmatics;
